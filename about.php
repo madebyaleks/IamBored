a28b70a36ba9cf0e8a5d5a1eea67f287cf81e1a7
Aleksander Naug <aleksander.naug@yahoo.com>
2013-09-05 22:42:48 +0200

    Now using CSS3 transitions in ALL browsers. Made the footera bit higher to compensate for small screens

--- about.php
+++ about.php
@@ -3,11 +3,13 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Internet roulette</title>
+	<title>About this useless site</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 	<meta name="description" value="Bored? Play internet roulette and get a random word, Imgur photo, Wikipedia article, subreddit and more">
 	<link rel="stylesheet" type="text/css" href="css/normalize.min.css">
 	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Raleway:700,500,100">
 	<link rel="stylesheet" type="text/css" href="css/iambored.css">
+	<link rel="icon" type="image/png" href="/images/favicon.png">
 </head>
 <body>
 <section class="everything">
@@ -27,9 +29,21 @@
 			<li>Repeat from step 2</li>
 		</ol>
 	</p>
+	<p>&nbsp;</p>
 </section>
 <footer>
 <p>A thing <a href="https://twitter.com/madebyaleks">Made by Aleks</a> | <a href="about">What is this?</a>
 </footer>
-</body
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-43820601-1', 'iambored.herokuapp.com');
+  ga('send', 'pageview');
+
+</script>
+</body>
 </html>
